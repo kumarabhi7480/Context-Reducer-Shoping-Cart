d60b9ef0d710523d1e9f2d5238e1c98dcd2fca84
Abhishek Kumar <kumarabhi7480>
2023-04-16 16:35:58 +0530

    update

---
+++ src/context/CartContext.js
@@ -0,0 +1,28 @@
+    import React, { useContext } from "react";
+import { createContext } from "react";
+
+
+    const initialState = {
+        cartList: [],
+        total: 0
+    }
+
+    const CartContext = createContext(initialState);
+
+    export const CartProvider = ({children}) =>{
+        const value = {
+            total: 10
+        };
+
+        return (
+            <CartContext.Provider value={value}>
+                {children}
+            </CartContext.Provider>
+        );
+
+    }
+
+    export const useCart = () =>{
+        const context = useContext(CartContext);
+        return context;
+    }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,11 @@
 import { useTitle } from "../hooks/useTitle";
 import { CartCard } from "../components";
+import { useCart } from "../context/CartContext";
 
 export const Cart = () => {
+
+  const { total } = useCart();
+
   useTitle("Cart");
   
   const products = [
@@ -12,7 +16,7 @@ export const Cart = () => {
   return (
     <main>
       <section className="cart">
-        <h1>Cart Items: {products.length}</h1>
+        <h1>Cart Items: {products.length} / ${total}</h1>
         { products.map((product) => (
           <CartCard key={product.id} product={product} />
         )) }        
